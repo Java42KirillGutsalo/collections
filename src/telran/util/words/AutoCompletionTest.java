efe36faf0f3eafa13fb6b8633be40aff33455d2b
Kirillgutsalo <kirya.sever11@gmail.com>
2022-01-16 13:28:26 +0200

    adds commit

---
+++ src/telran/util/words/AutoCompletionTest.java
@@ -0,0 +1,33 @@
+package telran.util.words;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AutoCompletionTest {
+	String words[]= {"abcdef","ab123","aaa","ab","ablmn","abbbb",
+			"a", "ABd","bbb", "B12"};
+	String wordsStartB[] = {"B12", "bbb"};
+	String wordsStartAB[] = {"ab","ab123","abbbb","abcdef","ABd","ablmn"};
+	String wordsStartABC[] = {"abcdef"};
+	AutoCompletion autoCompletion;
+	@BeforeEach
+	void setUp() throws Exception {
+		autoCompletion = new AutoCompletionImpl();
+		for(String word: words) {
+			autoCompletion.addWord(word);
+		}
+	}
+
+	@Test
+	void test() {
+		assertIterableEquals(Arrays.asList(wordsStartABC),
+				autoCompletion.getCompletionOptions("abc"));
+		assertIterableEquals(Arrays.asList(wordsStartB), autoCompletion.getCompletionOptions("B"));
+		assertIterableEquals(Arrays.asList(wordsStartAB), autoCompletion.getCompletionOptions("ab"));
+	}
+
+}
